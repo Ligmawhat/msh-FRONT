9d9f0dc4d469723f8aab66c4e84e76574e5d2a30
darsen <dlya_web_konkursa@mail.ru>
2022-06-10 23:57:40 +0300

    font size

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,13 +4,13 @@ import "./MainPage.scss"
 const MainPage: FC = () => {
   return (
     <div className="mainpage">
-      <div className="mainpage__landing landing">
+      <section className="mainpage__landing landing">
         <h1 className="landing__title">Стань волонтером прямо сейчас</h1>
         <div className="landing__buttons">
           <button className="landing__button">Регистрация</button>
           <button className="landing__button">Войти</button>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
