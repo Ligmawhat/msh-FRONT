e7c989361a5c3e7e689595dc1eccdac552174839
darsen <dlya_web_konkursa@mail.ru>
2022-06-10 23:36:27 +0300

    added routing && inited landing page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,7 +5,11 @@ const MainPage: FC = () => {
   return (
     <div className="mainpage">
       <div className="mainpage__landing landing">
-        <h1 className="landing__title">СТАТЬ ВОЛОНТЕРОМ ЛЕГКО!</h1>
+        <h1 className="landing__title">Стань волонтером прямо сейчас</h1>
+        <div className="landing__buttons">
+          <button className="landing__button">Регистрация</button>
+          <button className="landing__button">Войти</button>
+        </div>
       </div>
     </div>
   )
