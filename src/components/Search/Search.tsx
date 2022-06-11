0c15fb9fc004bd071e1a00889c8257d1bc1c907f
Алексей Рыбалкин <llllll99999@MacBook-Air-Aleksej.local>
2022-06-11 19:07:53 +0300

    searchbar with categories

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import Autocomplete from "@mui/material/Autocomplete"
+
+import TextField from "@mui/material/TextField"
+
+export default function Grouped() {
+  const options = top100Films.map((option) => {
+    const categoriesTitle = option.category.toUpperCase()
+    return {
+      categoriesTitle,
+      ...option,
+    }
+  })
+
+  return (
+    <Autocomplete
+      id="grouped-demo"
+      options={options.sort(
+        // eslint-disable-next-line prettier/prettier
+        (a, b) => -b.categoriesTitle.localeCompare(a.categoriesTitle)
+      )}
+      groupBy={(option) => option.categoriesTitle}
+      getOptionLabel={(option) => option.title}
+      sx={{ width: 300 }}
+      renderInput={(params) => (
+        <TextField {...params} label="With categories" />
+      )}
+    />
+  )
+}
+
+const top100Films = [
+  { title: "The Shawshank Redemption", year: 1994, category: "alex" },
+  { title: "The Godfather", year: 1972, category: "bilex" },
+  { title: "The Godfather: Part II", year: 1974, category: "cilex" },
+]
