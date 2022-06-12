dd56521d4e60d6acff020bd53114c9bd8f3d5345
darsen <dlya_web_konkursa@mail.ru>
2022-06-12 12:14:12 +0300

    deleted fake news

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,35 +1,10 @@
-import React, { FC, useEffect, useState } from "react"
+import React, { FC } from "react"
 import "./MainPage.scss"
-import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import Button from "../../components/common/Button/Button"
 
-//Удалить это mock
-interface NewsItem {
-  source: {
-    id: number
-    name: string
-  }
-  author: string
-  title: string
-  description: string
-  url: string
-  urlToImage: string
-  publishedAt: string
-  content: string
-}
-
-const apikey = "64569a53da124124b14e7cbbb71d210a"
-const api =
-  "https://newsapi.org/v2/everything?q=tesla&from=2022-05-10&sortBy=publishedAt&apiKey=64569a53da124124b14e7cbbb71d210a"
-
 const MainPage: FC = () => {
-  const [news, setNews] = useState<NewsItem[]>([])
   const navigate = useNavigate()
-
-  useEffect(() => {
-    axios.get(api).then((res) => setNews(res.data.articles))
-  }, [])
 
   return (
     <div className="mainpage">
@@ -42,23 +17,6 @@ const MainPage: FC = () => {
           <Button text={"Регистрация"} onClick={() => navigate("/register")} />
           <Button text={"Войти"} onClick={() => navigate("/login")} />
         </div>
-      </section>
-      <section className="mainpage__news news">
-        {news &&
-          news.slice(0, 9).map((el) => (
-            <div key={el.source.id} className="news__item">
-              <div className="news__left">
-                <img
-                  src={el.urlToImage || ""}
-                  alt="news img"
-                  className="news__img"
-                />
-              </div>
-              <div className="news__right">
-                <h2 className="news__title">{el.title}</h2>
-              </div>
-            </div>
-          ))}
       </section>
     </div>
   )
