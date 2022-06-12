8909fab52a3f6c83dc06c6f173ba29d8014cddce
darsen <dlya_web_konkursa@mail.ru>
2022-06-12 15:00:14 +0300

    added chats link to header

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,37 +1,35 @@
-import React, { useEffect, useRef } from "react"
+import React from "react"
 import Header from "../components/UI/Header/Header"
 import "./App.scss"
-import { useAppDispatch, useAppSelector } from "../hooks/redux"
 import Router from "../router/Router"
-import { useNavigate } from "react-router-dom"
 
 function App() {
-  const dispatch = useAppDispatch()
-  const url = process.env.REACT_APP_URL_SOCKET
-  const socket = useRef<WebSocket>()
-  const navigate = useNavigate()
-  const user = useAppSelector((state) => state.user)
-  const isCheckedAuth = useRef(false)
-
-  useEffect(() => {
-    if (user) {
-      // dispatch(getGames())
-      socket.current = new WebSocket(url as string)
-      // const socketOnMessage = createSocketOnMessage(dispatch)
-      // socket.current.onmessage = socketOnMessage
-      socket.current.onclose = () => {
-        alert("server close ws connection. Try again later")
-        navigate("/")
-      }
-    }
-
-    if (isCheckedAuth && !user) navigate("/register")
-  }, [user])
-
-  useEffect(() => {
-    // dispatch(checkUser())
-    isCheckedAuth.current = true
-  }, [])
+  // const dispatch = useAppDispatch()
+  // const url = process.env.REACT_APP_URL_SOCKET
+  // const socket = useRef<WebSocket>()
+  // const navigate = useNavigate()
+  // const user = useAppSelector((state) => state.user)
+  // const isCheckedAuth = useRef(false)
+  //
+  // useEffect(() => {
+  //   if (user) {
+  //     // dispatch(getGames())
+  //     socket.current = new WebSocket(url as string)
+  //     // const socketOnMessage = createSocketOnMessage(dispatch)
+  //     // socket.current.onmessage = socketOnMessage
+  //     socket.current.onclose = () => {
+  //       alert("server close ws connection. Try again later")
+  //       navigate("/")
+  //     }
+  //   }
+  //
+  //   if (isCheckedAuth && !user) navigate("/register")
+  // }, [user])
+  //
+  // useEffect(() => {
+  //   // dispatch(checkUser())
+  //   isCheckedAuth.current = true
+  // }, [])
 
   return (
     <div className="wrapper">
